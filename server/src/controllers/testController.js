569f9d20ce9400dee6690e1e8d58184e4e63b49d
Pawan Kumar <pawanandaman@gmail.com>
2024-03-03 20:06:15 +0530

    #4 added api to fetch user available tests

--- server/src/controllers/testController.js
+++ server/src/controllers/testController.js
@@ -4,6 +4,7 @@ const asyncErrors = require('../middleware/AsyncErrors');
 const Test = require('../models/test.models');
 const User = require('../models/user.models');
 const { isValidObjectId, isIdExists } = require('../utils/api/apiValidation');
+const UserTestAttempt = require('../models/userTestAttempt.models');
 
 /**
  * Fetch the test history of the user with given userId.
@@ -54,9 +55,71 @@ exports.getTestHistory = asyncErrors(async (req, res, next) => {
             totalTests: totalTests,
         };
         response = new ApiResponse(200, data);
-        return res.status(400).json(response);
+        return res.status(200).json(response);
     } catch (err) {
         message = `Failed to fetch the test history with userId ${userId}. Reason: ${err}`;
+        logger.error(message);
+        return next(err);
+    }
+});
+
+exports.getAvailableTests = asyncErrors(async (req, res, next) => {
+    const userId = req.params.userId;
+    logger.info(`RequestBody: ${userId}`);
+
+    if (!isValidObjectId(userId)) {
+        message = `UserId ${userId} is not valid.`;
+        logger.error(message);
+        const response = new ApiResponse(400, null, message);
+        return res.status(400).json(response);
+    }
+    const doesUserExists = await isIdExists(User, userId);
+
+    logger.info(`doesUser: ${doesUserExists}`);
+    if (!doesUserExists) {
+        message = `User with userId ${userId} does not exist.`;
+        logger.error(message);
+        const response = new ApiResponse(400, null, message);
+        return res.status(400).json(response);
+    }
+
+    try {
+        const availableTests = await Test.find({
+            users: { $elemMatch: { $eq: userId } },
+        });
+        logger.info('Available tests fetched successfully.');
+        logger.info(availableTests);
+
+        const formattedTests = [];
+
+        for (const test of availableTests) {
+            let userTestAttempt;
+            try {
+                userTestAttempt = await UserTestAttempt.findOne({
+                    user_id: userId,
+                    test_id: test._id,
+                });
+            } catch (err) {
+                const message = `Failed to fetch the user test attempted with test_id ${test._id}. Reason: ${err}`;
+                logger.error(message);
+                return next(err);
+            }
+
+            const status = userTestAttempt ? 'Completed' : 'Available';
+
+            formattedTests.push({
+                testId: test._id,
+                name: test.name,
+                subject: test.subject,
+                duration: test.duration.toString(), // Convert duration to string if needed
+                status: status,
+            });
+        }
+
+        const response = new ApiResponse(200, formattedTests);
+        return res.status(200).json(response);
+    } catch (err) {
+        const message = `Failed to fetch available tests for user with userId ${userId}. Reason: ${err}`;
         logger.error(message);
         return next(err);
     }
--- server/src/routes/testRouter.js
+++ server/src/routes/testRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { getTestHistory } = require('../controllers/testController');
+const { getAvailableTests } = require('../controllers/testController');
 
 const router = express.Router();
 
 router.route('/user/:userId/tests/history').get(getTestHistory);
+router.route('/user/:userId/tests/available').get(getAvailableTests);
 
 module.exports = router;
